613322d9b49c473fe721245c1ddb00e884c31b99
briancheung <bcheung5@gmail.com>
2015-08-24 17:55:21 -0700

    More progress on rconv work

--- peano/pops/convolutional.py
+++ peano/pops/convolutional.py
@@ -28,24 +28,29 @@ class Conv2D(Pop):
 
 class RecurrentConv2D(Pop):
     def __init__(self, n_filters, stack_size, n_row, n_col, 
-                    stride=(1,1), border_mode='valid'):
+                    stride=(1,1), border_mode='valid', activation_fn=T.tanh):
         W_init = sample_weights(stack_size*n_row*n_col, n_filters).T
         W_init = W_init.reshape(n_filters, stack_size, n_row, n_col)
         self.W_ih = theano.shared(W_init.astype(dtype))
-        self.b_ih = theano.shared(np.zeros(n_filters, dtype=dtype))
 
-        W_init = sample_weights(stack_size*n_row*n_col, n_filters).T
-        W_init = W_init.reshape(n_filters, stack_size, n_row, n_col)
+        W_init = sample_weights(n_filters*n_row*n_col, n_filters).T
+        W_init = W_init.reshape(n_filters, n_filters, n_row, n_col)
         self.W_hh = theano.shared(W_init.astype(dtype))
-        self.b_hh = theano.shared(np.zeros(n_filters, dtype=dtype))
 
-        self.params = [self.W_ih, self.b_ih,
-                        self.W_hh, self.b_hh]
+        self.b_h = theano.shared(np.zeros(n_filters, dtype=dtype))
+
+        self.params = [self.W_ih,
+                        self.W_hh,
+                        self.b_h]
 
         self.border_mode = border_mode
         self.stride = stride
+        self.activation_fn = activation_fn
 
-    def apply(self, x):
+    def step(self, x_t, h_tm1):
+        xWih = theano.sandbox.cuda.dnn.dnn_conv(x_t, self.W_ih, border_mode=self.border_mode, subsample=self.stride)
+        hWhh = theano.sandbox.cuda.dnn.dnn_conv(h_tm1, self.W_hh, border_mode=, stride=(1,1))
 
-        conv_val = theano.sandbox.cuda.dnn.dnn_conv(x, self.W, border_mode=self.border_mode, subsample=self.stride)
-        return conv_val + self.b.dimshuffle(0,'x','x')
+        h_t = self.activation_fn(xWih + hWhh + self.b_h.dimshuffle(0,'x','x'))
+
+        return h_t
